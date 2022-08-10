ce68b0d958bf7b3bdd23f71eb73d48b5573c7808
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-10 18:40:16 +0800

    Update HomeView.vue
    Rewrite home page contents
    - Adding new components header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <div class="container">
+      this is a test
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Header from '@/components/Header.vue'
 
 export default {
-  name: 'HomeView',
-  components: {
-    HelloWorld
+  name: 'Home',
+  components: { 
+    Header
   }
 }
 </script>
