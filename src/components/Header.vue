defbb3eea2627ccbd9e291ac91b8bbc01150eb75
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-10 18:36:13 +0800

    Create and modified Header.vue
    Structuring Header contents
    - Adding logo and  padding

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="text-center shadow-xl p-4 mb-10">
+    <div id="logo" class="bg-slate-300"></div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+header {
+  padding-top: 40px;
+  display: flex;
+  justify-content: center;
+  #logo {
+    width: 50px;
+    height: 50px;
+  }
+}
+</style>
