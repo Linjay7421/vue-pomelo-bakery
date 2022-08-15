72a1e4984a2daa22b1aac57c553965fb71bb6d74
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-15 17:11:17 +0800

    Update HomeView.vue
    Template and style changed
    - Add new component Footer
    - Add new css attribues for Footer and Header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,20 +1,29 @@
 <template>
-  <div class="home">
-    <Header></Header>
-    <div class="container">
-      this is a test
-    </div>
-  </div>
+  <Header></Header>
+  <div class="container"></div>
+  <Footer></Footer>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '@/components/Header.vue'
-
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
 export default {
-  name: 'Home',
-  components: { 
-    Header
-  }
-}
+  name: "Home",
+  components: {
+    Header,
+    Footer,
+  },
+};
 </script>
+
+<style lang="scss">
+Header {
+  position: fixed;
+  width: 100%;
+  top: 0;
+}
+Footer {
+  margin-top: 200px;
+}
+</style>
