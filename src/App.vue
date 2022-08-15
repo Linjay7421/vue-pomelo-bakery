060e305b905ae232298943c8c7f461c948c0db29
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-15 17:08:36 +0800

    Update App.vue
    Import new Font Family and default English font
    - default English font family to Arial
    - import TaipeiSansTCBeta

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,23 @@
 <template>
-  <router-view/>
+  <router-view />
 </template>
+
+<style>
+@font-face {
+  font-family: "TaipeiSansTCBeta-Regular";
+  src: url(@/assets/fonts/TaipeiSansTCBeta-Regular.ttf) format("opentype");
+}
+@font-face {
+  font-family: "TaipeiSansTCBeta-Bold";
+  src: url(@/assets/fonts/TaipeiSansTCBeta-Bold.ttf) format("opentype");
+}
+html {
+  font-size: 12px;
+}
+body {
+  font-family: Arial, sans-serif;
+}
+h1, h2, h3, h4 , h5 {
+  font-family: "TaipeiSansTCBeta-Bold";
+}
+</style>
