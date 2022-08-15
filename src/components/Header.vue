af4c469636e3a938d3b65504345619def77a0d8f
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-15 17:12:20 +0800

    Update Header.vue
    Structure rewrite
    - use flex and tailwind grid to curve Header structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,25 +1,41 @@
 <template>
   <header class="text-center shadow-xl p-4 mb-10">
-    <div id="logo" class="bg-slate-300"></div>
+    <ul class="grid grid-cols-5 gap-5">
+      <li>介紹</li>
+      <li>種類</li>
+      <li><img src="@/assets/images/logo.png" /></li>
+      <li>搜尋</li>
+      <li>訂單</li>
+    </ul>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'Header',
-  props: {}
-}
+  name: "Header",
+  props: {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 header {
-  padding-top: 40px;
-  display: flex;
-  justify-content: center;
-  #logo {
-    width: 50px;
-    height: 50px;
+  padding-top: 49px;
+  background-color: #fff;
+  > ul {
+    display: flex;
+    list-style: none;
+    justify-content: center;
+    align-items: flex-end;
+    padding: 0;
+    margin: 0;
+    > li {
+      padding: 2px 5px;
+      > img {
+        width: 50px;
+        height: 50px;
+      }
+    }
   }
 }
 </style>
