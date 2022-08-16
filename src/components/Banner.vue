e83efd2dbf66de14d8b75f65a60fe7bc95d45da4
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-17 00:33:08 +0800

    Create Banner.vue to components
    new component for home page
    - Title
    - Background with linear gradient

---
+++ src/components/Banner.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="Banner">
+    <div class="container">
+      <div class="title w-8/12">
+        <span>
+          <b>柚</b>
+          子是色胚，他真的很色 小心他去你家會跑來督你 。
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Banner",
+  props: {},
+  computed: {
+    elementHeight: function () {},
+  },
+};
+</script>
+
+<style lang="scss">
+.Banner {
+  width: 100vw;
+  height: 512px;
+  background: no-repeat;
+  background-size: 280%;
+  background-position: 50% 90%;
+  background-image: url("@/assets/images/banner.jpg");
+  -webkit-mask-image: linear-gradient(-180deg, rgba(255, 255, 255, 1) 68%, rgb(255, 255, 255, 0) 100%);
+  > .container {
+    .title {
+      font-size: 1.5rem;
+      b {
+        font-size: 2.5rem;
+      }
+    }
+  }
+}
+</style>
