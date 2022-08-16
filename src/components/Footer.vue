4057dd45f92233dd91645412531d60c215c37584
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-17 00:34:36 +0800

    Update Footer.vue
    Adjust position problem and bug
    - Center class information

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,55 +1,75 @@
 <template>
   <footer>
-    <h1>柚子麵包坊</h1>
-    <ul>
+    <div class="title">
+      <h1>柚子麵包坊</h1>
+    </div>
+    <ul class="informations">
       <li>0938-388-388</li>
       <li>10 AM ~ 10 PM</li>
       <li>台中市文心路上的某戶窗台上會有結石累累的柚子樹。</li>
     </ul>
-    <span>Copyright © 2022 Chin-Chieh, Lin . All rights reserved.</span>
+    <div class="copyRight">
+      <span>Copyright © 2022 Chin-Chieh, Lin . All rights reserved.</span>
+    </div>
   </footer>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Footer",
+  props: {}
+};
 </script>
 
 <style lang="scss">
 footer {
+  position: relative;
   background-color: #3f3f3f;
   color: #fff;
-  padding: 0px 40px;
-  > h1 {
+  padding: 30px 0px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100vw;
+  .title {
+    width: 100%;
     text-align: center;
     font-size: 1.5rem;
-    padding: 37px 0 28px 0;
+    margin-bottom: 28px;
   }
-  > ul {
+  .informations {
     > li {
-      font-size: 0.75rem;
       margin-bottom: 20px;
       max-width: 200px;
       word-wrap: break-word;
       &:nth-child(1) {
         &:before {
-            content: "電話："
+          font-family: "Font Awesome 5 Free"; font-weight: 900; content: "\f095";
+          margin-right: 5px;
         }
       }
       &:nth-child(2) {
         &:before {
-            content: "營業時間："
+          font-family: "Font Awesome 5 Free"; font-weight: 900; content: "\f017";
+          margin-right: 5px;
         }
       }
       &:last-of-type {
         &:before {
-            content: "地址："
+          font-family: "Font Awesome 5 Free"; font-weight: 900; content: "\f3c5";
+          margin-right: 5px;
         }
       }
     }
   }
-  > span {
-    font-size: 5px;
-    line-height: 1;
+  .copyRight {
+    text-align: center;
+    width: 100%;
+    > span {
+      font-size: 12px;
+      transform: scale(0.66);
+      display: inline-block;
+    } 
   }
 }
 </style>
