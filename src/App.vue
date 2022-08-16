157f1d3eb91dd7704ff80231d4a92751b60cc8cb
Linjay7421 <52476751+Linjay7421@users.noreply.github.com>
2022-08-17 00:29:48 +0800

    Update App.vue
    Rewrite body part
    - Add TaipeiSansTCBeta-Regular to font family for default Chinese setting

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 </template>
 
 <style>
+@import url(https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css);
 @font-face {
   font-family: "TaipeiSansTCBeta-Regular";
   src: url(@/assets/fonts/TaipeiSansTCBeta-Regular.ttf) format("opentype");
@@ -15,9 +16,11 @@ html {
   font-size: 12px;
 }
 body {
-  font-family: Arial, sans-serif;
+  overflow-x: hidden;
+  font-family: Arial, TaipeiSansTCBeta-Regular,sans-serif;
 }
 h1, h2, h3, h4 , h5 {
   font-family: "TaipeiSansTCBeta-Bold";
+  display: block;
 }
 </style>
